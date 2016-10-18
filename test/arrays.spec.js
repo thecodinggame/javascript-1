e42f7be0302419ea1f9737d550785f49b19f583e
Eric Mignot <eric.mignot@gmail.com>
2016-10-18 14:41:08 -0400

    more about adding elements in arrays

--- test/arrays.spec.js
+++ test/arrays.spec.js
@@ -18,7 +18,32 @@ describe("Arrays", function() {
 
 			expect(array[1]).to.equal('green me!');
 		});
+				
+		it("'unshift' adds at the beggining", function() {
+			array.unshift(one);
+			array.unshift(two);
 
+			expect(array[0]).to.equal('green me!');
+		});
+
+		it("'splice' offers insertion", function() {
+			array.push(one);
+			array.push(two);
+			var bird = { value : "bird" };
+			array.splice(0, 1, bird);
+
+			expect(array[0]).to.equal('green me!');
+		});
+
+		it("'splice' offers multi-insertion", function() {
+			array.push(one);
+			array.push(two);
+			var fox = { value : "fox" };
+			var cat = { value : "cat" };
+			array.splice(1, 2, fox, cat);
+
+			expect(array[1]).to.equal('green me!');
+			expect(array[2]).to.equal('green me!');
+		});
 	});
-
 });
